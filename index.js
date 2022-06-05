69e71fda812ace8fdd5e34884c05e37c1d7c1c70
Kiattipan Muangyai <57340445+icest99@users.noreply.github.com>
2022-06-06 03:40:21 +0700

    add comment

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const URL_POSTS = 'https://maqe.github.io/json/posts.json'
 
 async function getData() {
     try {
+        // fetch data
         const authors = await axios.get(URL_AUTHORS);
         const posts = await axios.get(URL_POSTS);
         let allData = []
 
+        //combining data from 2 api request Posts & Authors. 
+        //into easier to work with format
         posts.data.forEach((post, i) => {
             allData.push(post)
 
@@ -18,8 +21,7 @@ async function getData() {
             })
         })
 
-        console.log(allData)
-
+        //create html template using handlebars.
         createHTML(allData)
     } catch (error) {
         console.log(error);
@@ -28,6 +30,7 @@ async function getData() {
 
 getData();
 
+//handlebars.js custom helper to convert time format
 Handlebars.registerHelper("convertTime", (created_at) => {
     let date = new Date(created_at);
     let d = moment(created_at).format("dddd, MMMM D, YYYY, HH:mm")
@@ -40,5 +43,6 @@ function createHTML(allData) {
     document.querySelector("#output").innerHTML = fillTemplate
 }
 
+//get user's timezone
 const tzid = Intl.DateTimeFormat().resolvedOptions().timeZone;
 document.querySelector(".timezone").innerHTML = tzid
